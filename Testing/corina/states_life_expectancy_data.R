16f3bf5f8a5681a5f008c0c2833acef7cd6ba979
corinaramont <corinaramont@tamu.edu>
2022-09-13 22:00:32 -0500

    adds CDC data, rearranges CDC data, cleans column names of life_expect

--- Testing/corina/states_life_expectancy_data.R
+++ Testing/corina/states_life_expectancy_data.R
@@ -1,10 +1,17 @@
+library(dplyr)
+library(tidyr)
+
 #reading in life expectancy data
 life1 = read.csv("~/capstone_project/datasets/life-expectancy-1959-1978.csv")
 life2 = read.csv("~/capstone_project/datasets/life-expectancy-1979-1998.csv")
 life3 = read.csv("~/capstone_project/datasets/life-expectancy-1999-2016.csv")
+life4 = read.csv("~/capstone_project/datasets/cdc-life-expectancy-2018-2020.csv")
+
+life4 = life4[,-4]
+life4 = life4%>%pivot_wider(names_from=YEAR, values_from=RATE)%>%arrange(STATE)
 
 #combines all the life expectancy data into one dataframe life_expect
-life_expect = cbind(life1,life2[,-1],life3[,-1])
+life_expect = cbind(life1,life2[,-1],life3[,-1],life4[,4],life4[,3],life4[,2])
 
 #clean column names
-colnames(life_expect) = c("State",1959:2016)
+colnames(life_expect) = c("State",1959:2016,2018:2020)
