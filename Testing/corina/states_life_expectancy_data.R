7353dc44a16f99f7575d1d0cbd77e9dce9e974c1
corinaramont <corinaramont@tamu.edu>
2022-09-14 18:54:40 -0500

    calculates theoretical life expectancy for 2017 using linear regression from existing data, fills in for missing column

--- Testing/corina/states_life_expectancy_data.R
+++ Testing/corina/states_life_expectancy_data.R
@@ -22,11 +22,20 @@ for(i in 1959:2020){
 colnames(life_expect) = c("State",year_names)
 
 #check for existing missing values (before we add 2017)
-table(is.na(life_expect))
+table(is.na(life_expect)) #there are no other missing values
 
 #missing column for 2017 - add values to it
-mean_state_life_expect = round(rowMeans(life_expect[,c(-1,-60)]),digits=1)
-life_expect$year_2017 = mean_state_life_expect
+fill_year_2017 = c()
+for(j in 1:50){
+  y = unlist(life_expect[j,c(-1,-60)])
+  x = c(1959:2016,2018:2020) 
+  info = lm(y~x)
+  life_expect_2017 = info$coefficients[1] + (info$coefficients[2]*2017)
+  fill_year_2017 = c(fill_year_2017,life_expect_2017)
+}
+
+#adds calculated data values from regression into year 2017 column
+life_expect$year_2017 = round(fill_year_2017,digit=1)
 
 #saves life_expect dataframe to an Rda file
 save(life_expect,file="~/capstone_project/datasets/life_expectancies_1959_2020.Rda")
