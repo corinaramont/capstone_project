cb0b143c19fad17e3af4905a081c92ab71908b21
Sam <sam.j.currans@gmail.com>
2022-09-15 17:54:20 -0500

    added comments

--- Testing/sam/EPAdata.R
+++ Testing/sam/EPAdata.R
@@ -2,48 +2,44 @@ library(RAQSAPI)
 
 #aqs_sign_up(email = "sam_currans@tamu.edu")
 
+
 aqs_credentials(username = "sam_currans@tamu.edu", key = "russetosprey91 ")
 
 
+#Gets the State codes in format that can be pasted into API calls
+cleanedstatecodes = (paste("'", continentalstates$stateFIPS, "'", sep = "",collapse = ","))
 
+#When making API calls use this in the sta
+#Gets the  codes for each state
 statecodes = aqs_states()
 
+
+#Shortens the FIPS data to only the 48 continental states 
+continentalstates FIPS= statecodes[1:which(statecodes$state == "Wyoming"),][c(-which(statecodes$state == "Alaska"),-which(statecodes$state == "Hawaii"),-which(statecodes$state == "District Of Columbia") ),]
+teFIPS parameter below 
+# cat("c(",cleanedstatecodes,")", sep = "")
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+#For checking the county codes for Texas
 texascounties = aqs_counties_by_state(stateFIPS = 48)
 
-
+#Gets the CO data from bexar county Texas, near San Antonio
 bexarcounties = aqs_dailysummary_by_county(parameter = "42101",
                                            bdate = as.Date("20220101", format = "%Y%m%d"),
                                            edate = as.Date("20220201", format = "%Y%m%d"),
                                            stateFIPS = "48",
-                                           countycode = "29")
+                                           countycode = "029")
 
 
-sleep(5)
-temp = aqs_dailysummary_by_county(parameter = "42101",
-                                  bdate = as.Date("20160101", format = "%Y%m%d"),
-                                  edate = as.Date("20170228", format = "%Y%m%d"),
-                                  stateFIPS = "48",
-                                  countycode = "029")
-head(temp)
-
-HA = aqs_monitors_by_state(parameter="42101",
-                           bdate=as.Date("20170101",
-                                         format="%Y%m%d"),
-                           edate=as.Date("20171231",
-                                         format="%Y%m%d"),
-                           stateFIPS="02"
-)
-
-colnames(HA)
-dim(HA)
 
 
-TX = aqs_monitors_by_state(parameter=c("14129","42101"),
+firsttwostates = aqs_monitors_by_state(parameter=c("42101","14129"),
                            bdate=as.Date("19800101",
                                          format="%Y%m%d"),
                            edate=as.Date("20220101",
                                          format="%Y%m%d"),
-                           stateFIPS=c("48","01")
+                           stateFIPS=c("01","04")
 )
 
 head(TX)
