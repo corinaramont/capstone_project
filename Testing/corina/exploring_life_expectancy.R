13ec3e35875d2a42b30e91920d8bdb082fbe691a
corinaramont <corinaramont@tamu.edu>
2022-09-14 22:23:35 -0500

    adds a region column and attempt to graph region data

--- Testing/corina/exploring_life_expectancy.R
+++ Testing/corina/exploring_life_expectancy.R
@@ -13,27 +13,34 @@ ggplot(data=life_expect, aes(x=Year, y=Life_Expectancies, group=State)) +
 region1 = life_expect%>%filter(State %in% c("CT", "ME", "MA", "NH", 
                                             "RI", "VT", "NJ", "NY", "PA"))
 region1_life_expect = life_expect%>%group_by(Year)%>%
-  summarise(avg_by_year=mean(Life_Expectancies))
+  summarise(avg_by_year=mean(Life_Expectancies))%>%mutate(Region="Northeast")
 
 #region 2 - midwest
 region2 = life_expect%>%filter(State %in% c("IL", "IN", "MI",
                                             "OH", "WI", "IA", "KS", "MN",
                                             "MO", "NE", "ND", "SD"))
 region2_life_expect = life_expect%>%group_by(Year)%>%
-  summarise(avg_by_year=mean(Life_Expectancies))
+  summarise(avg_by_year=mean(Life_Expectancies))%>%mutate(Region="Midwest")
 
 #region 3 - south
 region3 = life_expect%>%filter(State %in% c("DE","FL","GA","MD","NC",
                                             "SC","VA", "WV","AL","KY",
                                             "MS","TN","AR","LA","OK","TX"))
 region3_life_expect = life_expect%>%group_by(Year)%>%
-  summarise(avg_by_year=mean(Life_Expectancies))
+  summarise(avg_by_year=mean(Life_Expectancies))%>%mutate(Region="South")
 
 #region 4 - west
 region4 = life_expect%>%filter(State %in% c("AZ","CO","ID","MT","NV","NM",
                                             "UT","WY","AK","CA","HI","OR","WA"))
 
 region4_life_expect = life_expect%>%group_by(Year)%>%
-  summarise(avg_by_year=mean(Life_Expectancies))
+  summarise(avg_by_year=mean(Life_Expectancies))%>%mutate(Region="West")
 
 #new data frame for US state life expectancies by region
+regions_life_expect = rbind(region1_life_expect,region2_life_expect,
+                            region3_life_expect,region4_life_expect)
+
+#graph for life expectancies by region
+ggplot(data=regions_life_expect, aes(x=Year, y=avg_by_year, group=Region)) + 
+  geom_line(aes(color=Region)) + geom_point(aes(color=Region)) + 
+  ggtitle("US Regions Life Expectancies 1959-2020") + ylab("Life Expectancies (Years)")
