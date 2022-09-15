3f4e4d778d34137525facc66ce27c3bb04e3a899
corinaramont <corinaramont@tamu.edu>
2022-09-14 20:50:31 -0500

    edits life expectancy data to have rownames

---
+++ Testing/corina/exploring_life_expectancy.R
@@ -0,0 +1,3 @@
+#loads life expectancy data into file
+load("~/capstone_project/datasets/life_expectancies_1959_2020.Rda")
+
--- Testing/corina/states_life_expectancy_data.R
+++ Testing/corina/states_life_expectancy_data.R
@@ -13,13 +13,16 @@ life4 = life4%>%pivot_wider(names_from=YEAR, values_from=RATE)%>%arrange(STATE)
 
 #combines all the life expectancy data into one dataframe life_expect
 life_expect = cbind(life1,life2[,-1],life3[,-1],c(rep(0,50)),life4[,4],life4[,3],life4[,2])
+state_names = life_expect[,1]
+life_expect = life_expect[,-1]
+rownames(life_expect) = state_names
 
 #clean column names
 year_names = c()
 for(i in 1959:2020){
   year_names = c(year_names,paste("year_",toString(i),sep=""))
 }
-colnames(life_expect) = c("State",year_names)
+colnames(life_expect) = c(year_names)
 
 #check for existing missing values (before we add 2017)
 table(is.na(life_expect)) #there are no other missing values
@@ -27,7 +30,7 @@ table(is.na(life_expect)) #there are no other missing values
 #missing column for 2017 - add values to it
 fill_year_2017 = c()
 for(j in 1:50){
-  y = unlist(life_expect[j,c(-1,-60)])
+  y = unlist(life_expect[j,-59])
   x = c(1959:2016,2018:2020) 
   info = lm(y~x)
   life_expect_2017 = info$coefficients[1] + (info$coefficients[2]*2017)
