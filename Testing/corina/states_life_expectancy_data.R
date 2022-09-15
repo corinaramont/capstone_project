7ac93eb2a36d2f6e800b38de5905db10513526b2
corinaramont <corinaramont@tamu.edu>
2022-09-14 21:20:52 -0500

    makes life_expect dataset to be pivot_long, resaves data

--- Testing/corina/states_life_expectancy_data.R
+++ Testing/corina/states_life_expectancy_data.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(tidyr)
 library(tibble)
+library(stringr)
 
 #reading in life expectancy data
 life1 = read.csv("~/capstone_project/datasets/life-expectancy-1959-1978.csv")
@@ -13,16 +14,14 @@ life4 = life4%>%pivot_wider(names_from=YEAR, values_from=RATE)%>%arrange(STATE)
 
 #combines all the life expectancy data into one dataframe life_expect
 life_expect = cbind(life1,life2[,-1],life3[,-1],c(rep(0,50)),life4[,4],life4[,3],life4[,2])
-state_names = life_expect[,1]
-life_expect = life_expect[,-1]
-rownames(life_expect) = state_names
+life_expect[,1] = str_trim(life_expect[,1],side="right")
 
 #clean column names
 year_names = c()
 for(i in 1959:2020){
   year_names = c(year_names,paste("year_",toString(i),sep=""))
 }
-colnames(life_expect) = c(year_names)
+colnames(life_expect) = c("State",year_names)
 
 #check for existing missing values (before we add 2017)
 table(is.na(life_expect)) #there are no other missing values
@@ -30,7 +29,7 @@ table(is.na(life_expect)) #there are no other missing values
 #missing column for 2017 - add values to it
 fill_year_2017 = c()
 for(j in 1:50){
-  y = unlist(life_expect[j,-59])
+  y = unlist(life_expect[j,c(-1,-60)])
   x = c(1959:2016,2018:2020) 
   info = lm(y~x)
   life_expect_2017 = info$coefficients[1] + (info$coefficients[2]*2017)
@@ -38,7 +37,12 @@ for(j in 1:50){
 }
 
 #adds calculated data values from regression into year 2017 column
-life_expect$year_2017 = round(fill_year_2017,digit=1)
+life_expect[,60] = round(fill_year_2017,digit=1)
+
+life_expect = life_expect %>% 
+  pivot_longer(cols = c(2:63),names_to="Year",values_to="Life_Expectancies")
+
+life_expect$Year = rep(1959:2020,50)
 
 #saves life_expect dataframe to an Rda file
 save(life_expect,file="~/capstone_project/datasets/life_expectancies_1959_2020.Rda")
