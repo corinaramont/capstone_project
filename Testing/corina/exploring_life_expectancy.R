0ce604f3058f1f3aeb90a78cc7ce4927177f0bb8
corinaramont <corinaramont@tamu.edu>
2022-09-15 09:35:30 -0500

    fixes data to actually be separated by region

--- Testing/corina/exploring_life_expectancy.R
+++ Testing/corina/exploring_life_expectancy.R
@@ -5,6 +5,7 @@ library(ggplot2)
 #loads life expectancy data into file
 load("~/capstone_project/datasets/life_expectancies_1959_2020.Rda")
 
+#graph for all states life expectancies 1959-2020
 ggplot(data=life_expect, aes(x=Year, y=Life_Expectancies, group=State)) + 
   geom_line(aes(color=State)) + geom_point(aes(color=State)) + 
   ggtitle("US States Life Expectancies 1959-2020") + ylab("Life Expectancies (Years)")
@@ -12,28 +13,28 @@ ggplot(data=life_expect, aes(x=Year, y=Life_Expectancies, group=State)) +
 #region 1 - northeast "CO", "ME", "MA", "NH", "RI", "VT", "NJ", "NY", "PA"
 region1 = life_expect%>%filter(State %in% c("CT", "ME", "MA", "NH", 
                                             "RI", "VT", "NJ", "NY", "PA"))
-region1_life_expect = life_expect%>%group_by(Year)%>%
+region1_life_expect = region1%>%group_by(Year)%>%
   summarise(avg_by_year=mean(Life_Expectancies))%>%mutate(Region="Northeast")
 
 #region 2 - midwest
 region2 = life_expect%>%filter(State %in% c("IL", "IN", "MI",
                                             "OH", "WI", "IA", "KS", "MN",
                                             "MO", "NE", "ND", "SD"))
-region2_life_expect = life_expect%>%group_by(Year)%>%
+region2_life_expect = region2%>%group_by(Year)%>%
   summarise(avg_by_year=mean(Life_Expectancies))%>%mutate(Region="Midwest")
 
 #region 3 - south
 region3 = life_expect%>%filter(State %in% c("DE","FL","GA","MD","NC",
                                             "SC","VA", "WV","AL","KY",
                                             "MS","TN","AR","LA","OK","TX"))
-region3_life_expect = life_expect%>%group_by(Year)%>%
+region3_life_expect = region3%>%group_by(Year)%>%
   summarise(avg_by_year=mean(Life_Expectancies))%>%mutate(Region="South")
 
 #region 4 - west
 region4 = life_expect%>%filter(State %in% c("AZ","CO","ID","MT","NV","NM",
                                             "UT","WY","AK","CA","HI","OR","WA"))
 
-region4_life_expect = life_expect%>%group_by(Year)%>%
+region4_life_expect = region4%>%group_by(Year)%>%
   summarise(avg_by_year=mean(Life_Expectancies))%>%mutate(Region="West")
 
 #new data frame for US state life expectancies by region
