fcc6ec2fb486464d00582f8dcfdf2c456c42c638
corinaramont <corinaramont@tamu.edu>
2022-10-09 12:01:21 -0500

    create function to retrieve annualmeans by year, state, param

--- Testing/corina/random_intercepts.R
+++ Testing/corina/random_intercepts.R
@@ -1,1 +1,15 @@
 library(lme4)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+setwd("~/capstone_project/datasets")
+load("clean_pollutant_life_data.Rda")
+
+pollcol = function(years, states, param){
+  return((cleaned_pollutants_data%>%
+            filter(year %in% years, state == states, parameter == param))$annualmean)
+}
+
+
+#lmer(lifeexpect ~  ,data=bama)
