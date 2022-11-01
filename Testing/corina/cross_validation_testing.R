534f4c120038c8136bdc437b44a9b32b54e00c5d
corinaramont <corinaramont@tamu.edu>
2022-10-31 21:31:21 -0500

    splitting dataset and testing

--- Testing/corina/cross_validation_testing.R
+++ Testing/corina/cross_validation_testing.R
@@ -1,13 +1,13 @@
 library(tidyverse)
 library(caret) #cross validation methods
+library(lme4)
 
 #load dataset Y
 load("datasets/scrub_daddied_dataset.Rda")
 
 Y_new = na.exclude(Y)
-#model
-model = lmer(LifeExpect ~ CO + NO2 + PM2.5 + Year + (1|state), 
-             data = Y_new)
+#origmodel 
+#= lmer(LifeExpect ~ CO + NO2 + PM2.5 + Year + (1|state), data = Y_new)
 
 #validation set approach
 set.seed(123)
@@ -16,4 +16,15 @@ set.seed(123)
 random_sample = createDataPartition(Y_new$LifeExpect,
                                      p = 0.8, list = F)
 
-training_data  <- Y_new[random_sample,]
+training_data = Y_new[random_sample,]
+testing_data = Y_new[-random_sample,]
+
+model = lmer(LifeExpect ~ CO + NO2 + PM2.5 + Year + (1|state), 
+             data = training_data)
+
+predictions = predict(model, testing_data)
+
+data.frame(R2 = R2(predictions, testing_data$LifeExpect),
+            RMSE = RMSE(predictions, testing_data$LifeExpect),
+            MAE = MAE(predictions, testing_data$Life_Expect))
+
